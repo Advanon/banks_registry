5839add4033fd7f30eac017702840ca995345a63
Vlad Gramuzov <vlad.gramuzov@gmail.com>
2018-12-19 20:48:53 +0300

    feat(banks-registry): Update dependencies

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe BanksRegistry::VERSION do
   it 'Then returns a correct version number' do
-    expect(BanksRegistry::VERSION).to eq('0.1.0')
+    expect(BanksRegistry::VERSION).to eq('0.1.1')
   end
 end
